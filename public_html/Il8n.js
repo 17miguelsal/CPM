90c17eaa31a2806d5fb473b7462543fb91f3e93c
migue <migue@DESKTOP-0981FPK>
2023-06-07 10:36:57 +0200

    1.4

--- public_html/Il8n.js
+++ public_html/Il8n.js
@@ -2,10 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-function cambioIdioma(){
+function cambioIdioma() {
     var select = document.getElementById("selectorIdioma");
-  var selectedOption = select.options[select.selectedIndex].value;
+    var selectedOption = select.options[select.selectedIndex].value;
 
-  window.location.href = selectedOption;
-
+    if (selectedOption === 'en-GB') {
+        window.location.href = "Subscription_en.html";
+    }
+    if (selectedOption === 'es-ES'){
+        window.location.href = "Subscripcion_es.html";
+    }
 }
