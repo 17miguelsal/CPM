cf68dc2e8149d52efda98df9f31b13ccd801d412
migue <migue@DESKTOP-0981FPK>
2023-06-08 10:23:06 +0200

    1.4

--- public_html/Il8n.js
+++ public_html/Il8n.js
@@ -13,3 +13,24 @@ function cambioIdioma() {
         window.location.href = "Subscripcion_es.html";
     }
 }
+
+function muestraDatos(id, id2){
+    var dato = document.getElementById(id);
+    var paragraph = document.getElementById(id2);
+
+    var number = dato.value;
+    if (number) {
+        var N = new Number(number);
+        /* ReqI13 */
+        paragraph.innerHTML = '(' + decimalFormat.format(N) + ')';
+    } else
+        paragraph.innerHTML = '';
+}
+function calcular(idDinero, idFecha){
+     var dinero = document.getElementById(idDinero);
+     var fecha = document.getElementById(idFecha);
+     var fechaHoy = new Date();
+     
+     dinero.innerHTML = monedaFormat.format(162,22);
+     fecha.innerHTML = dateShortFormat.format(fechaHoy) +" ///// " + dateLongFormat.format(fechaHoy);
+}
