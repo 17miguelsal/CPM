35cff2fe805277b40c271c2140dad2606f1abf41
migue <migue@192.168.1.134>
2023-06-08 15:25:03 +0200

    1.5

--- public_html/Il8n.js
+++ public_html/Il8n.js
@@ -13,7 +13,7 @@ function cambioIdioma() {
         window.location.href = "Subscripcion_es.html";
     }
 }
-
+/* ReqI12 */
 function muestraDatos(id, id2){
     var dato = document.getElementById(id);
     var paragraph = document.getElementById(id2);
@@ -26,11 +26,41 @@ function muestraDatos(id, id2){
     } else
         paragraph.innerHTML = '';
 }
-function calcular(idDinero, idFecha){
+/* ReqI12 */
+function muestraAprobado(idBloqs, idExpcs, idBool){
+    var bloq = document.getElementById(idBloqs);
+    var ex = document.getElementById(idExpcs);
+    var bool = document.getElementById(idBool);
+    
+    var b = bloq.value;
+    var e = ex.value;
+    
+    if(b>12){
+        /* ReqI13 */
+        bool.innerHTML = decimalFormat.format(e+=0.5);
+    } else{
+        /* ReqI13 */
+        bool.innerHTML = decimalFormat.format(e-=0.5);
+    }
+}
+/* ReqI12 */
+function calcular(idDinero, idFecha, idAdm, idBloqs, idCFinal){
      var dinero = document.getElementById(idDinero);
      var fecha = document.getElementById(idFecha);
-     var fechaHoy = new Date();
      
-     dinero.innerHTML = monedaFormat.format(162,22);
-     fecha.innerHTML = dateShortFormat.format(fechaHoy) +" ///// " + dateLongFormat.format(fechaHoy);
+     var admin = document.getElementById(idAdm);
+     var coste = document.getElementById(idCFinal);
+     var bloq = document.getElementById(idBloqs);
+     
+     var fechaMañana = new Date(Date.now() + 1000 * 60 * 60 * 24 * 1);
+     var segundaMatricula = new Number(162.22);
+     var gastoAdmin = new Number(3);
+     var bloques = bloq.value;
+     
+     dinero.innerHTML = monedaFormat.format(segundaMatricula);
+     admin.innerHTML = monedaFormat.format(gastoAdmin)+ "por cada bloque";
+     /* ReqI14 */
+     coste.innerHTML = monedaFormat.format(segundaMatricula + gastoAdmin * bloques);
+     /* ReqI15, ReqI16 */
+     fecha.innerHTML = dateShortFormat.format(fechaMañana) +" ///// " + dateLongFormat.format(fechaMañana);
 }
